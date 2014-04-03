47a9436144bf966216ff7837e95996ea14d0f826
Denis Barishev <denz@twiket.com>
2014-04-03 18:52:30 +0700

    changed to sysbench

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,18 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-url = 'http://downloads.sourceforge.net/project/systester/systester/1.5.0/systester-1.5.1-linux-amd64.tar.gz?r=http%3A%2F%2Fsystester.sourceforge.net%2Fdownloads.php&ts=1396513529'
-
-remote_file '/root/systester.tar.gz' do
-  source url
-end
+package "sysbench"
 
 bash "ha" do
   code(<<-EOH
     cd /root
-    test -d systester-* || tar xzf systester.tar.gz
-    cd systester-*
-    nohup ./systester-cli -threads 16 -turns 30000 -log
+    nohup sysbench --num-threads=8 --max-time=14400 --force-shutdown=0 --test=cpu --cpu-max-prime=99999 run > /root/sysbench.cpu 2>&1
   EOH
   )
 end
